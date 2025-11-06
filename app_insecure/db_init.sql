80811ada4e55268302c7e205d0ae32bc2657ef6b
Daniel Moradi <74241363+danielmoradi1@users.noreply.github.com>
2025-11-06 19:03:00 +0000

    Initial commit

---
+++ app_insecure/db_init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE,
+  password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  product_id INTEGER,
+  text TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT OR IGNORE INTO users (username, password) VALUES ('admin', 'password');
+INSERT OR IGNORE INTO products (name, description) VALUES ('Macbook pro m4', 'Det är jo också en dator');
